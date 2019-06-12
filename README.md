64ea799a7b40dc48360e2f9acb9b265e2633b51a
shaojianheng <630909067@qq.com>
2019-06-12 10:07:24 +0800

    2.0版本ok

--- README.md
+++ README.md
@@ -1,24 +1,23 @@
 
 ## usage
-Usage: ssgit [options]
+Usage: ssgit [options] or ssgit message[string]
 
 选项：
-  --version   &nbsp; &nbsp;       显示版本号  &nbsp; &nbsp;[布尔]
+  --target, -t &nbsp; &nbsp;       target branch, default value is t &nbsp; &nbsp;                [字符串]
 
-  --target, -t    &nbsp; &nbsp;       target branch &nbsp; &nbsp;   [字符串] [必需] [默认值: "t"]
-  
-  --source, -s    &nbsp; &nbsp;       source branch  &nbsp; &nbsp;  [字符串] [必需]
+  --conflict, -c &nbsp; &nbsp;     Whether conflicts have been resolved  &nbsp; &nbsp;              [布尔]
 
-  --commitAndMerge, -m  &nbsp; &nbsp;    message about commit&merge  &nbsp; &nbsp;   [字符串] [必需]
+  --build, -b  &nbsp; &nbsp;       Whether to pack locally, default value is false &nbsp; &nbsp;    [布尔]
 
-  --conflict, -c    &nbsp; &nbsp;     Whether conflicts have been resolved  &nbsp; &nbsp; [字符串] [必需] [默认值: false]
+  --defalutValue, -d&nbsp; &nbsp;  get the current default value  &nbsp; &nbsp;                     [布尔]
 
-  --build, -b    &nbsp; &nbsp;        Whether to pack locally  &nbsp; &nbsp; [字符串] [必需] [默认值: false]
+  --tdefalut  &nbsp; &nbsp;        set default value for the target branch &nbsp; &nbsp;          [字符串]
 
-  -h, --help     &nbsp; &nbsp;        显示帮助信息  &nbsp; &nbsp;  [布尔]
+  --bdefalut &nbsp; &nbsp;         set default value for whether to package locally &nbsp; &nbsp;   [布尔]
+
+  -h, --help&nbsp; &nbsp;          显示帮助信息  &nbsp; &nbsp;                                     [布尔]
+
+  -v, --version &nbsp; &nbsp;      显示版本号 &nbsp; &nbsp;                                         [布尔]
 
 示例：
-  ssgit -t=t -s=feature/search -m='commit'
-
-
-
+  ssgit fix:change number
--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const jsonfile = require("jsonfile");
 const chalk = require("chalk");
 const file = `${__dirname}/default.json`;
 let jsonObj = jsonfile.readFileSync(file);
-
+//设置命令行参数
 const argv = require("yargs")
 	.option("target", {
 		alias: "t",
@@ -23,37 +23,65 @@ const argv = require("yargs")
 		describe: "Whether to pack locally, default value is false",
 		type: "boolean"
 	})
+	.option("defalutValue", {
+		alias: "d",
+		describe: "get the current default value ",
+		boolean: true,
+		type: "boolean"
+	})
 	.option("tdefalut", {
 		describe: "set default value for the target branch",
 		type: "string"
 	})
 	.option("bdefalut", {
 		describe: "set default value for whether to package locally ",
-		type: "boolean",
-		boolean: true
+		type: "boolean"
 	})
 	.usage("Usage: ssgit [options] or ssgit message[string]")
 	.example(" ssgit fix:change number ")
 	.alias("h", "help")
 	.alias("v", "version").argv;
-//更改默认值
-if (argv.bdefalut || argv.tdefalut) {
-	if (argv.tdefalut) {
+//获取当前所在分支,必须要使用trim,否则会出现意外的字符换行,导致脚本不能正常运行
+argv.s = shell.exec("git rev-parse --abbrev-ref HEAD").trim();
+//修改默认值
+if (argv.tdefalut !== undefined || argv.bdefalut !== undefined) {
+	if (argv.tdefalut !== undefined) {
 		jsonObj = { ...jsonObj, target: argv.tdefalut };
 	}
-	if (argv.bdefalut) {
+	if (argv.bdefalut !== undefined) {
 		jsonObj = { ...jsonObj, build: argv.bdefalut };
 	}
 	jsonfile.writeFileSync(file, jsonObj);
+	console.log(chalk.green.bold("modify has been applied"));
+	let obj = {};
+	obj.target = jsonObj.target;
+	obj.build = jsonObj.build;
+	console.log("current defalut Value is: ", obj);
+	return;
 }
-//获取当前所在分支,必须要使用trim,否则会出现意外的字符换行,导致脚本不能正常运行
-argv.s = shell.exec("git rev-parse --abbrev-ref HEAD").trim();
-argv.t = argv.t || jsonObj.target;
-argv.b = argv.b === undefined ? jsonObj.build : argv.b;
+//输出默认值
+if (argv.d) {
+	let obj = {};
+	obj.target = jsonObj.target;
+	obj.build = jsonObj.build;
+	console.log("defalutValue: ", obj);
+	return;
+}
+//设置目标分支
+argv.t = argv.t!==undefined?argv.t:jsonObj.target;
+if (!argv.c) {
+	//未发生冲突时,要随时更新prevtarget
+	jsonObj = { ...jsonObj, prevtarget: argv.s };
+} else {
+	//发生冲突时,target的值要从prevtarget取
+	argv.t = jsonObj.prevtarget;
+}
+//设置是否本地打包
+argv.b = argv.b !== undefined ? argv.b:jsonObj.build;
 //设置提交信息
 argv.m = argv._.join(" ");
-//非配置默认
-if (!(argv.bdefalut || argv.tdefalut)) {
+//切换分支操作提交信息必填
+if (!(argv.bdefalut !== undefined || argv.tdefalut !== undefined || argv.defalutValue)) {
 	if (argv._.length === 0) {
 		throw new Error(chalk.red.bold("Please enter commit information "));
 	}
@@ -63,8 +91,8 @@ let order = "";
 if (argv.c) {
 	order = `${
 		argv.b
-			? "npm run build&&git add -A&&git commit  -m " + argv.m + "&&git push&&git checkout " + argv.s
-			: "(git add -A&&git commit  -m " + argv.m + "&&git push&&git checkout " + argv.s + ")||(git push&&git checkout " + argv.s + ")"
+			? "npm run build&&git add -A&&git commit  -m " + argv.m + "&&git push&&git checkout " + argv.t
+			: "(git add -A&&git commit  -m " + argv.m + "&&git push&&git checkout " + argv.t + ")||(git push&&git checkout " + argv.t + ")"
 	}`;
 } else {
 	order = `(git pull&&
@@ -85,6 +113,11 @@ if (argv.c) {
     git push&&
     git checkout ${argv.s})`;
 }
-// console.log(chalk.red(`${argv.b} ${argv.c} ${argv.m}`));
+//输出命令到控制台
 console.log(chalk.green(`${order}`));
-shell.exec(order)
+//执行命令
+shell.exec(order);
+if (!argv.c) {
+	//更新文件中的prevtarget的值,将写操作放在末尾,可以避免阻塞分支的切换
+	jsonfile.writeFileSync(file, jsonObj);
+}
